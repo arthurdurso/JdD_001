19603357e448c4b07dd1635fbf27618b903ca273
Durso <dursoarthur@gmail.com>
2024-07-31 21:51:18 -0300

    desafio completo

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-print("hello world")
+# 1) Solicita ao usuário que digite seu nome
+nome = input("Digite seu nome: ")
 
-print("version 2.0")
+# 2) Solicita ao usuário que digite o valor do seu salário
+# Converte a entrada para um número de ponto flutuante
+salario = float(input("\nDigite o valor do seu salario: "))
+
+# 3) Solicita ao usuário que digite o valor do bônus recebido
+# Converte a entrada para um número de ponto flutuante
+bonus = float(input("\nDigite o valor do seu bonus: "))
+
+# 4) Calcule o valor do bônus final
+bonus_final = 1000 + salario + bonus
+
+# 5) Imprima cálculo do KPI para o usuário
+print(f"\nCalculo do KPI: {bonus_final}")
+
+# 6) Imprime a mensagem personalizada incluindo o nome do usuário, salário e bônus
+print(f"\nInformacoes do Usuario\nNome: {nome}\nSalario: {salario}\nBonus: {bonus}")
+
+# Bônus: Quantos bugs e riscos você consegue identificar nesse programa?
+print("\nOs bugs e riscos é a possibilidade de Typeerror vindo do input salario e bonyus, não conseguindo transformar em float, caso nao venha um numero.")
